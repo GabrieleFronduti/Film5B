d2bb88b92bc3ca64de0369ed3290304172d35262
gabrielefronduti <gabriele.fronduti17@gmail.com>
2021-12-17 17:56:57 +0100

    1

---
+++ Film5B/app/src/main/java/com/example/film5b/activity_dettaglio_film.java
@@ -0,0 +1,14 @@
+package com.example.film5b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_dettaglio_film extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dettaglio_film);
+    }
+}
